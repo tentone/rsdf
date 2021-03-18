4b92d41dfe432f5c6458a711eddf320fe56e65ac
José Ferrão <tentone@outlook.com>
2021-03-18 00:24:36 +0000

    Tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+use rand::Rng;
+use rand::rngs::ThreadRng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Input: ");
+
+    let mut guess: String = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let mut rng: ThreadRng = rand::thread_rng();
+    let secret: i32 = rng.gen_range(1..10);
+
+    println!("You guessed {} it was {}", guess, secret);
 }
