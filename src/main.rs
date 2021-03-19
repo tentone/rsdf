b38788ebdddb7e30ca463c0b0031bbf8d84dad97
José Ferrão <tentone@outlook.com>
2021-03-19 11:44:07 +0000

    Learning rust

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ fn main() {
     let mut guess: String = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let mut guess_number: i32 = guess.parse().unwrap();
+
     let mut rng: ThreadRng = rand::thread_rng();
     let secret: i32 = rng.gen_range(1..10);
 
-    println!("You guessed {} it was {}", guess, secret);
+    let correct: bool = secret == guess_number
+
+    println!("You guessed {} it was {} {}", guess, secret, correct);
 }
