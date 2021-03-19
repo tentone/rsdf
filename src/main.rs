a59eea2d0c5cd7cd099045ffdffa8e00aabd794e
José Ferrão <tentone@outlook.com>
2021-03-19 22:25:14 +0000

    Window

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use std::io;
 use rand::Rng;
 use rand::rngs::ThreadRng;
+extern crate sdl2;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Input: ");
+    // let mut guess: String = String::new();
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    //
+    // let mut rng: ThreadRng = rand::thread_rng();
+    // let secret: i32 = rng.gen_range(1..10);
 
-    let mut guess: String = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let sdl = sdl2::init().unwrap();
 
-    let mut guess_number: i32 = guess.parse().unwrap();
+    let video_subsystem = sdl.video().unwrap();
 
-    let mut rng: ThreadRng = rand::thread_rng();
-    let secret: i32 = rng.gen_range(1..10);
+    let _window = video_subsystem
+        .window("Game", 640, 480)
+        .resizable()
+        .build()
+        .unwrap();
 
-    let correct: bool = secret == guess_number
+    let mut event_pump = sdl.event_pump().unwrap();
 
-    println!("You guessed {} it was {} {}", guess, secret, correct);
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,  _ => {}
+            }
+        }
+    }
 }
