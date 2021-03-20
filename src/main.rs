338413e2d3e74510c55406f546cf72ef68c5d93f
José Ferrão <tentone@outlook.com>
2021-03-20 18:54:44 +0000

    Derp

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use std::io;
-use rand::Rng;
-use rand::rngs::ThreadRng;
+use sdl2::{VideoSubsystem, Sdl, EventPump};
+
 extern crate sdl2;
 
 fn main() {
-    // let mut guess: String = String::new();
-    // io::stdin().read_line(&mut guess).expect("Failed to read line");
-    //
-    // let mut rng: ThreadRng = rand::thread_rng();
-    // let secret: i32 = rng.gen_range(1..10);
-
-    let sdl = sdl2::init().unwrap();
-
-    let video_subsystem = sdl.video().unwrap();
+    let sdl: Sdl = sdl2::init().unwrap();
+    let video_subsystem: VideoSubsystem = sdl.video().unwrap();
 
     let _window = video_subsystem
         .window("Game", 640, 480)
@@ -20,7 +12,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut event_pump = sdl.event_pump().unwrap();
+    let mut event_pump: EventPump = sdl.event_pump().unwrap();
 
     'main: loop {
         for event in event_pump.poll_iter() {
