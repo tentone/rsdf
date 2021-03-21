58c06c27e7e60c3ba4d95485037362cf3d5669b8
José Ferrão <tentone@outlook.com>
2021-03-21 17:25:58 +0000

    Window okay

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# rsdf
-Rust sdf rendering experiments.
+# Rust SDF
+- Small SDF renderer experiment using Rust and OpenGL (via glow)
+  - Support booth desktop OpenGL and WebGL trough web assembly.
+- To run the project you will need to install/compile [GLFW](https://www.glfw.org/docs/latest/compile.html)
+  - In windows you can install [Msys2](https://www.msys2.org/) and [CMake](https://cmake.org/download/) to get these dependencies working.
+
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-use sdl2::{VideoSubsystem, Sdl, EventPump};
-
-extern crate sdl2;
+extern crate glium;
 
 fn main() {
-    let sdl: Sdl = sdl2::init().unwrap();
-    let video_subsystem: VideoSubsystem = sdl.video().unwrap();
+    use glium::{glutin, Surface};
 
-    let _window = video_subsystem
-        .window("Game", 640, 480)
-        .resizable()
-        .build()
-        .unwrap();
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let mut event_pump: EventPump = sdl.event_pump().unwrap();
+    event_loop.run(move |ev, _, control_flow| {
 
-    'main: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit { .. } => break 'main,  _ => {}
-            }
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
         }
-    }
+    });
 }
