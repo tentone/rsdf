018d95a7b681d117014abfd9a82a6a13b81d1af1
José Ferrão <tentone@outlook.com>
2021-03-21 18:58:37 +0000

    Triangle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,77 @@
 extern crate glium;
+extern crate glutin;
+
+use glium::Display;
+use glutin::window::WindowBuilder;
+use glutin::event_loop::EventLoop;
+use glutin::{ContextBuilder, NotCurrent};
+use std::time::Instant;
+use glutin::event::Event;
+use glium::Surface;
+use glium::implement_vertex;
+
+// Structure to represent a vertex
+#[derive(Copy, Clone)]
+struct Vertex {
+    // Position of the vertex in x, y
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
 
 fn main() {
-    use glium::{glutin, Surface};
+    let window: WindowBuilder = glutin::window::WindowBuilder::new();
 
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+    let context: ContextBuilder<NotCurrent> = glutin::ContextBuilder::new();
 
-    event_loop.run(move |ev, _, control_flow| {
+    let event_loop: EventLoop<()> = glutin::event_loop::EventLoop::new();
 
+    let display: Display = glium::Display::new(window, context, &event_loop).unwrap();
+
+    let shape: Vec<Vertex> = vec![
+        Vertex{position: [-0.5, -0.5]},
+        Vertex{position: [0.0,  0.5]},
+        Vertex{position: [ 0.5, -0.25]}
+    ];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader: &str = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader: &str = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
+
+    event_loop.run(move |event: Event<()>, _, control_flow| {
         let mut target = display.draw();
+
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+
         target.finish().unwrap();
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+        let next_frame: Instant = std::time::Instant::now() + std::time::Duration::from_millis(16);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame);
 
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-        match ev {
+        match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
