b703e633c4aa42302f1679cbc55d22f4da142d81
José Ferrão <tentone@outlook.com>
2021-04-01 11:47:04 +0100

    Derp

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ if cargo "$@"; then
     [ -d "$DIR/target/debug" ] && cp -r "$DIR/resources" "$DIR/target/debug/resources"
     [ -d "$DIR/target/release" ] && cp -r "$DIR/resources" "$DIR/target/release/resources"
 fi
+
