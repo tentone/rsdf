a9804282d81f3fa6c6128a933e0a0514fd2948f2
tentone <tentone@outlook.com>
2021-04-12 08:03:32 +0100

    Linux run

--- run.sh
+++ run.sh
@@ -3,11 +3,7 @@
 cargo build
 
 DIR="$(dirname "$0")"
-
-if cargo "$@"; then
-    [ -d "$DIR/target/debug" ] && cp -r "$DIR/resources" "$DIR/target/debug/resources"
-    [ -d "$DIR/target/release" ] && cp -r "$DIR/resources" "$DIR/target/release/resources"
-fi
+cp -r "./src/textures" "./target/debug/textures"
 
 cd target/debug
 ./rsdf
